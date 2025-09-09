159982323dc9ce50df64193feff6a374c47721f3
Rallabandi Varshith <136003240+varshithanonymous@users.noreply.github.com>
2025-09-09 22:13:51 +0530

    Time: 7 ms (87.99%) | Memory: 17.9 MB (69.64%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -0,0 +1,16 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [""] * numRows
+        curr_row = 0
+        going_down = False
+
+        for ch in s:
+            rows[curr_row] += ch
+            if curr_row == 0 or curr_row == numRows - 1:
+                going_down = not going_down
+            curr_row += 1 if going_down else -1
+
+        return "".join(rows)
